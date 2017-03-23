b338d0d228359fa179b821361d242573c340079f
Bear <william456821@gmail.com>
2017-03-23 20:49:13 +0800

    project 大改

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-project',
@@ -7,43 +10,62 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
-  skill = [
-    {
-      title :"inQ官方網站",
-      subTitle : "Angular 2 寫成",
-      content : "<p><a target='_blank' href='https://bear1110.github.io/demo-website/'>官方網站Link</p>",
-      time : '2015~現在',
-      icon : 'fa fa-globe fa-5x'
-    },{
-      title :"戰艦棋",
-      subTitle : "Java-SocketProgram(TCP/UDP)",
-      content : "<p>使用Java 寫成 有附聊天室 <a target='_blank' href='https://github.com/Bear1110/Java-SocketProgram'>Github</a></p>",
-      time : '2015~現在',
-      icon : 'fa fa-ship fa-5x'
-    },{
-      title :"中央大學-問卷系統",
-      subTitle : "Laravel 結合 vue.js",
-      content : "<p>使用Laravel  做為 後端平台並結合 Vue.js 寫成 <a target='_blank' href='http://140.115.19.197/QuestionSys/'>Link</a></p>",
-      time : '2015~現在',
-      icon : 'fa fa-quora fa-5x'
-    },{
-      title :"中央大學-新生資訊網",
-      subTitle : "Laravel 為基礎而完成",
-      content : "<p>使用 Laravel 做為 整個平台的框架寫成，由於每年都會換新，我所做的那年已經下架。</p><p>不過此處有我錄製的  <a target='_blank' href='https://drive.google.com/drive/folders/0B9pxsA4Zx9Q8WkJhMkwwNTZoYXM?usp=sharing'>Demo 影片 Link</a> 只附上我有參與的部分</p>",
-      time : '2015~現在',
-      icon : 'fa fa-newspaper-o fa-5x'
-    },{
-      title :"大三專題-AR 貪食蛇 ( 製作中 )",
-      subTitle : "Androind 平台下去設計 正在進行中",
-      content : "<p>Android Studio 開發中，我負責的部分為 繪製地圖、TCP連線。</p>",
-      time : '2015~現在',
-      icon : 'fa fa-android fa-5x'
-    }
+  skill = [{
+    title: "What-does-the-box-say (軟體工程課程團隊專案)",
+    subTitle: "遊戲內容為 TCP/UDP 2D地圖連線遊戲，為 7 人合作所完成",
+    content: `<p>途中練習使用團隊開發方式</p>
+    <p>像是<b>design各種遊戲需求</b>(像是遊戲中有<b>子彈飛行</b>設計)後訂定<b>明確的Spec</b>，<b>Coding Style</b></p><p>並且練習如何使用Debug工具及使用<b>JUnit Test</b></p>
+    <img src="assets/img/box.png" class="img-responsive" width="500" height="250">
+    <p>我主要負責的是一些TCP的玩家<b>邏輯參數管理(秒數部分)</b>，及<b>JUnit Test Case 撰寫</b></p>
+      <p><a target='_blank' href='https://github.com/Bear1110/What-does-the-box-say'>Github專案Link</a></p>`,
+    time: '2015~現在',
+    icon: 'fa fa-archive fa-5x'
+  }, {
+    title: "國立中央大學-問卷系統",
+    subTitle: "Laravel 結合 vue.js 來做動態的新增與修改 (包含跳題功能及各種彈性設計)",
+    content: `<p>與同學一同並使用 Git 當作版控系統，與同學前後花約一年的時間，從零到有完成此系統</p>
+      <p><b>即時更新、跳題邏輯、問卷防呆</b> 等等 都是使用 Vue.js完成複雜需求 <a target='_blank' href='http://140.115.19.197/QuestionSys/'>問卷系統連結</a></p>`,
+    time: '2015~現在',
+    icon: 'fa fa-quora fa-5x'
+  }, {
+    title: "戰艦棋 (計算機網路課程實作專案)",
+    subTitle: "Java-SocketProgram(TCP/UDP) 程式碼約 1500行 點我可看程式精簡UML圖",
+    content: `<p>遊戲流程:先擺好自己的船 之後猜對方的船隻方位。</p>
+        <p>附有聊天室功能方便溝通互動，與同學協力完成 </p>
+      <p>主要負責<b>連線MVC邏輯部分撰寫</b>
+      <img src="assets/img/UML.png" class="img-responsive" width="1241" height="944" >
+      <a target='_blank' href='https://github.com/Bear1110/Java-SocketProgram'>Github</a></p>`,
+    time: '2015~現在',
+    icon: 'fa fa-ship fa-5x'
+  }, {
+    title: "AR 貪食蛇 ( 大三專題-製作中 )",
+    subTitle: "想達到人與人外出跑步運動的AR貪食蛇，創意Android 遊戲App",
+    content: `<p>獲得GPS或是陀螺儀的部分</p>
+                  <p>連線架構( 如玩家的資料型態，TCP protocal協定) 都是由我負責完成。</p>
+                  <p>小地圖的座標繪製等等</p>`,
+    time: '2015~現在',
+    icon: 'fa fa-android fa-5x'
+  }, {
+    title: "inQ官方網站",
+    subTitle: "Angular 2 寫成，具有RWD功能、Router、GooleMapApi，方便維護及增修資料",
+    content: `<p>完全拆分成component切版，方便維護及增修改資料。
+                  <a target='_blank' href='https://bear1110.github.io/demo-website/'>官方網站Link</p>`,
+    time: '2015~現在',
+    icon: 'fa fa-globe fa-5x'
+  }, {
+    title: "中央大學-新生資訊網",
+    subTitle: "中央大學每年為了新進學生所架設的新生專屬網站，Laravel 為基礎而完成",
+    content: "<p>內容包含大大小小知識及Q&A發問區，使用 Laravel 做為 整個平台的框架寫成，由於每年都會換新，我所做的那年已經下架。</p><p>不過此處有我錄製的  <a target='_blank' href='https://drive.google.com/drive/folders/0B9pxsA4Zx9Q8WkJhMkwwNTZoYXM?usp=sharing'>Demo 影片 Link</a> 只附上我有參與的部分</p>",
+    time: '2015~現在',
+    icon: 'fa fa-newspaper-o fa-5x'
+  }, {
+    title: "統一發票兌獎系統 (組合語言程式)",
+    subTitle: "可彈性輸入 兌獎號碼 ， 並且 輸入 發票後三碼來完成兌獎",
+    content: "<p>使用組合語言完成 ，為課程之實作<a target='_blank' href='https://github.com/Bear1110/assembly-language-Lotto'>Github專案Link</a></p>",
+    time: '2015~現在',
+    icon: 'fa fa-terminal fa-5x'
+  }]
 
-  ]
-  constructor() { }
-  ngOnInit() {
-  }
-
-
+  constructor() {}
+  ngOnInit() {}
 }
